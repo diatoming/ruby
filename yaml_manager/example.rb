aaa8240d5051ae1252eaaca4be8ee48a2e998cfa
rimbaud1854 <rimbaud1854_91@hotmail.com>
2012-07-17 15:22:51 +0200

    yaml manager added
    simple lib to manage yaml files

---
+++ yaml_manager/example.rb
@@ -0,0 +1,31 @@
+#!/usr/local/bin/ruby -w
+#--
+# Have faith in the way things are.
+#
+# example.rb
+# v.: 0.0.1
+#++
+
+# == Description
+# example on how to use YAMLManager
+#
+# == Usage
+# ruby example.rb
+#
+# == Author
+# rimbaud1854
+#
+# == Copyright
+# Copyright (c) 2012 RimbaudCode
+# Licensed under GPLv3+. No warranty provided.
+
+require './lib/yaml_manager'
+
+filename  = 'test.txt'
+data      = {one: 'string', two: 'another string'}
+
+manager = YAMLManager.new
+manager << filename
+manager.write_data data
+p manager.read_data
+
