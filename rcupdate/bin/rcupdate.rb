37788fe9af72f98c78b2542f82c5fd5da59a3ba1
rimbaudcode <rimbaud1854_91@hotmail.com>
2012-07-30 20:27:08 +0200

    icloud not allowed to be launched from terminal

--- rcupdate/bin/rcupdate.rb
+++ rcupdate/bin/rcupdate.rb
@@ -32,7 +32,7 @@ apps = ['AppFresh.app', 'App Store.app', 'Dropbox.app', 'GitHub.app',
 special_apps = [
   '/Applications/SugarSync Manager.app/Contents/MacOS/SugarSyncManager',
   '/Applications/TeX/TeX Live Utility.app/Contents/MacOS/TeX Live Utility',
-  '/System/Library/PreferencePanes/iCloudPref.prefPane',
+  #'/System/Library/PreferencePanes/iCloudPref.prefPane',
   ]
   
 tools = ['brew update', 'brew upgrade']
@@ -41,8 +41,6 @@ apps.each do |app| RCSys.launch_app app end
 special_apps.each do |app| RCSys.launch_special_app app end
 tools.each do |tool| RCSys.launch_tool tool end
 p 'don\'t forget to update gems and gems packages'
-#p 'also run skydrive...'
 p 'also macruby...'
-#p 'also github reps...'
-#p 'also icloud... ;)'
+p 'also icloud... ;)'
 p 'done...'
