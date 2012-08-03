26177186884f3f590fa94ab00909dbd8d8012fdb
rimbaudcode <rimbaud1854.91@gmail.com>
2012-08-03 18:04:03 +0200

    test project added

---
+++ learning/test/test.rb
@@ -0,0 +1,23 @@
+#!/usr/local/bin/ruby -w
+#--
+# Have faith in the way things are.
+#
+# test.rb
+# v.: 0.0.1
+#++
+
+# == Description
+# test project
+#
+# == Usage
+# test.rb
+#
+# == Author
+# rimbaud1854
+#
+# == Copyright
+# Copyright (c) 2012 RimbaudCode
+# Licensed under GPLv3+. No warranty provided.
+
+p 'this is a test!'
+
