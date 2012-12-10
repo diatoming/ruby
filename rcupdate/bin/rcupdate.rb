2218a86b80bac1347597a8a1f97a7326b0bde7f9
rimbaud1854 <rimbaud1854.91@gmail.com>
2012-12-10 10:16:00 +0100

    rcupdate updated ;)

--- rcupdate/bin/rcupdate.rb
+++ rcupdate/bin/rcupdate.rb
@@ -27,15 +27,12 @@ $LOAD_PATH << File.expand_path(File.dirname(__FILE__) + '/../lib')
 require 'lib_rcsys'
 
 apps = [
-  #'AppFresh.app', 
-  'App Store.app', 'Dropbox.app', 'GitHub.app', 
-  'SkyDrive.app', 'Xmarks for Safari.app'
+  'App Store.app', 'Dropbox.app', 'GitHub.app', 'SkyDrive.app', 
+  'Xmarks for Safari.app', 'SugarSync.app'
   ]
   
 special_apps = [
-  '/Applications/SugarSync Manager.app/Contents/MacOS/SugarSyncManager',
   '/Applications/TeX/TeX Live Utility.app/Contents/MacOS/TeX Live Utility',
-  #'/System/Library/PreferencePanes/iCloudPref.prefPane',
   ]
   
 tools = ['brew update', 'brew upgrade']
@@ -45,5 +42,4 @@ special_apps.each do |app| RCSys.launch_special_app app end
 tools.each do |tool| RCSys.launch_tool tool end
 p 'don\'t forget to update gems and gems packages'
 p 'also macruby...'
-#p 'also icloud... ;)'
 p 'done...'
