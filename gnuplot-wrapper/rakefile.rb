5ac4aa67cfd5d17ad7e72574b7f99cd12a1135ec
rimbaudcode <rimbaud1854.91@gmail.com>
2013-05-17 17:39:32 +0200

    gnuplot-wrapper: improved: error checking added

--- gnuplot-wrapper/rakefile.rb
+++ gnuplot-wrapper/rakefile.rb
@@ -23,20 +23,87 @@
 # Copyright (c) 2012 rimbaudcode
 # Licensed under GPLv3+. No warranty provided.
 
+
+require 'rake/clean'
+
+
 # this file contains the ruby-gnuplot info
 GNUPLOT_RUBY = 'sine.rb'
+
+# pre-requisite tools
+GNUPLOT_PATH  = `which gnuplot`.chomp
+PDFLATEX_PATH = `which pdflatex`.chomp
 
 # define filenames based on input file: GNUPLOT_RUBY
 GNUPLOT_TEX = File.basename(GNUPLOT_RUBY, '.rb')
 WRAPPER_TEX = 'plot-wrapper.tex'
 WRAPPER_PDF = File.basename(WRAPPER_TEX, 'tex') << 'pdf'
-PDF_OUTPUT  = File.basename(GNUPLOT_RUBY, 'rb') << 'pdf'
+OUTPUT_PDF  = File.basename(GNUPLOT_RUBY, 'rb') << 'pdf'
 
-PDFLATEX_PATH = `which pdflatex`.chomp
+# tasks
+DEFAULT_TASKS = [
+  :check_environment,
+  :build_gnuplot_tex, 
+  :build_wrapper_tex,
+  :build_wrapper_pdf,
+  :wrapper_to_final_pdf,
+  :clean,
+  #:open_pdf
+  ]
+
+desc 'get a pdf file from ruby-gnuplot data'
+task :default => DEFAULT_TASKS
+
+desc 'check if the environment is correctly set'
+task :check_environment do
+  FileDoesNotExistError   = Class.new StandardError
+  NoGNUPlotInstalledError = Class.new StandardError
+  NoTeXDistributionError  = Class.new StandardError
+  begin
+    raise FileDoesNotExistError unless File.file? GNUPLOT_RUBY
+  rescue
+    puts "File: #{GNUPLOT_RUBY} doesn't exist."
+    exit 1
+  end
+  begin
+    raise NoGNUPlotInstalledError if GNUPLOT_PATH.empty?
+  rescue
+    puts "GNUPlot not found. Install GNUPlot."
+    exit 1
+  end
+  begin
+    raise NoTeXDistributionError if PDFLATEX_PATH.empty?
+  rescue
+    puts "No TeX distribution found. Install TeX."
+    exit 1
+  end
+end
+
+desc 'build a raw latex picture (plot) using ruby-gnuplot data'
+task :build_gnuplot_tex => GNUPLOT_TEX
+
+desc 'wrap the raw latex picture with a compiling latex file'
+task :build_wrapper_tex => WRAPPER_TEX
+
+desc 'compile latex wrapper to pdf'
+task :build_wrapper_pdf => WRAPPER_PDF
+
+desc 'rename latex wrapper to output pdf'
+task :wrapper_to_final_pdf => OUTPUT_PDF
+
+desc 'open output pdf'
+task :open_pdf do
+  `open #{OUTPUT_PDF}`
+end
+
+# maintenance tasks
+CLEAN.include('*.tex', '*.aux', '*.log')
+CLOBBER.include('*.pdf')
+
 
 # instructions to build the necessary files
 # produce raw graphic using gnuplot
-file GNUPLOT_TEX => GNUPLOT_RUBY do 
+file GNUPLOT_TEX => GNUPLOT_RUBY do
   `ruby #{GNUPLOT_RUBY}`
 end
 
@@ -62,45 +129,6 @@ file WRAPPER_PDF => WRAPPER_TEX do
   `#{PDFLATEX_PATH} #{WRAPPER_TEX}` 
 end
 
-# tasks
-DEFAULT_TASKS = [:build_gnuplot_tex, 
-  :build_wrapper_tex,
-  :build_wrapper_pdf
-  #:rename_pdf,
-  #:clean,
-  #:open_pdf
-  ]
-
-desc 'get a pdf file from ruby-gnuplot data'
-task :default => DEFAULT_TASKS
-
-desc 'build a raw latex picture (plot) using ruby-gnuplot data'
-task :build_gnuplot_tex => GNUPLOT_TEX
-
-desc 'wrap the raw latex picture with a compiling latex file'
-task :build_wrapper_tex => WRAPPER_TEX
-
-desc 'compile latex wrapper to pdf'
-task :build_wrapper_pdf => WRAPPER_PDF
-
-desc 'rename pdf output with input file name'
-task :rename_pdf do
-  filename = File.basename(TMP_LATEX_FILE, 'tex') << 'pdf'
-  `mv #{filename} #{PDF_OUTPUT}`
-end
-
-desc 'open pdf output'
-task :open_pdf do
-  `open #{PDF_OUTPUT}`
-end
-
-
-desc 'clean latex auxiliary files'
-task :clean do
-  `rm *.tex *.aux *.log`
-end
-
-desc 'clean latex auxiliary files and pdfs'
-task :cleanall => :clean do
-  `rm *.pdf`
+file OUTPUT_PDF => WRAPPER_PDF do 
+  mv WRAPPER_PDF, OUTPUT_PDF
 end
