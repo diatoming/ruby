bbeb24ac4a65461889cccd4cdda02106a8b1c650
rimbaudcode <rimbaud1854.91@gmail.com>
2013-06-25 19:45:45 +0200

    doc improved

--- tmpdir/tmpdir.rb
+++ tmpdir/tmpdir.rb
@@ -2,24 +2,20 @@
 #--
 # Have faith in the way things are.
 #
-# rctmpdir.rb
-# v.: 0.0.1
+# tmpdir.rb
+# date: 2013.06.25
 #++
 
 # == Description
 # locate and open the tmp folder in OSX (Lion)
 #
-# == Usage
-# rctmpdir.rb
-#
 # == Author
 # rimbaud1854
 #
 # == Copyright
-# Copyright (c) 2012 RimbaudCode
+# Copyright (c) 2013 rimbaudcode
 # Licensed under GPLv3+. No warranty provided.
 
-#require 'FileUtils'
 require 'tmpdir'
 
 tmp = Dir.tmpdir
