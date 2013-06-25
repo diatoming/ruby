c6419b961d635783a95ebde7df09b5f97ec29181
rimbaudcode <rimbaud1854.91@gmail.com>
2013-06-25 19:35:42 +0200

    main method added

--- mkplaylist/bin/mkplaylist.rb
+++ mkplaylist/bin/mkplaylist.rb
@@ -24,21 +24,26 @@ $LOAD_PATH << File.expand_path(File.dirname(__FILE__) + '/../lib')
 
 require 'mkplaylist'
 
+PLAY_LIST_NAME  = 'playlist.m3u'
+LABEL_COLOR     = 1
+MEDIA_FILES_EXT = ['.flv','.mp4', '.MP4', '.mp3', '.avi', '.MP3', '.MOV',
+                    '.m4a', '.wmv', '.mov', '.m4v','.mpg', '.f4v']
+
+def main args
+  rm_old_playlist
+  file_lst = find_files_with_extensions MEDIA_FILES_EXT
+  write_playlist file_lst, PLAY_LIST_NAME
+  label_file PLAY_LIST_NAME, LABEL_COLOR
+  exit
+end
+
 if $0 == __FILE__
   begin
-    PLAY_LIST_NAME  = 'playlist.m3u'
-    LABEL_COLOR     = 4
-    media_files_ext = ['.flv','.mp4', '.MP4', '.mp3', '.avi', '.MP3', '.MOV',
-                        '.m4a', '.wmv', '.mov', '.m4v','.mpg', '.f4v']
-
-    rm_old_playlist
-    file_lst = find_files_with_extensions media_files_ext
-    write_playlist file_lst, PLAY_LIST_NAME
-    label_file PLAY_LIST_NAME, LABEL_COLOR
+    exit main $*
   rescue
     $stderr.puts "#{$!}"
     $@.each do |item| $stderr.puts item end
-    exit 1
+    abort
   ensure
     #
   end
